97e914ac1c55f49bcfe83f15e116575ac4b5c65d
seokhyeonChung <44970596+seokhyeonChung@users.noreply.github.com>
2022-02-16 13:38:59 +0900

    Time: 7 ms (86.50%), Space: 168.4 MB (45.04%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,25 +1,17 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int N = nums.length;
-        if (N == 1) {
-            return nums;
+        int idx = 0;
+        int n = nums.length;
+        int[] result = new int[n];
+        for(int num : nums){
+            if(num < pivot) result[idx++] = num;
         }
-        
-        int[] res = new int[N];
-        int l = -1, r = N;
-        //collect all elements smaller than pivot
-        for (int i = 0; i < N; i++) {
-            if (nums[i] < pivot) {
-                res[++l] = nums[i];
-            }
-            if (nums[N - 1 - i] > pivot) {
-                res[--r] = nums[N - 1 - i];
-            }
+        for(int num : nums){
+            if(num == pivot) result[idx++] = num;
         }
-        //fill the pivot elements if any
-        for (int i = l + 1; i <= r - 1; i++) {
-            res[i] = pivot;
+        for(int num : nums){
+            if(num > pivot) result[idx++] = num;
         }
-        return res;
+        return result;
     }
 }
