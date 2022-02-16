d8dcc17f4165c7990422b1ddccb206a9323bbcd8
seokhyeonChung <44970596+seokhyeonChung@users.noreply.github.com>
2022-02-16 13:29:51 +0900

    Time: 7 ms (86.50%), Space: 169 MB (33.40%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,24 +1,12 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        LinkedList<Integer> list1 = new LinkedList<>();
-        LinkedList<Integer> list2 = new LinkedList<>();
-        for(int i =0; i<nums.length; i++){
-            if(nums[i] < pivot){
-                list1.add(nums[i]);
-            } else if(nums[i] > pivot){
-                list2.add(nums[i]);
-            } else{
-                list2.addFirst(nums[i]);
-            }
-        }
-        int[] result = new int[nums.length];
-        int idx = 0;
-        while(!list1.isEmpty()){
-            result[idx++] = list1.poll();
-        }
-        while(!list2.isEmpty()){
-            result[idx++] = list2.poll();
-        }
-        return result;
-    }
+	int i=0, n= nums.length, ans[]= new int[n];
+	for(int num:nums)
+		if(num<pivot) ans[i++]= num;
+	for(int num:nums)
+		if(num==pivot) ans[i++]= num;
+	for(int num:nums)
+		if(num>pivot) ans[i++]= num;
+	return ans;
+}
 }
