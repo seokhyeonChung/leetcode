9e842c65e3c1122bb5f007cc5010b81d1aba6e05
seokhyeonChung <44970596+seokhyeonChung@users.noreply.github.com>
2022-02-22 13:32:09 +0900

    Time: 0 ms (100.00%), Space: 46 MB (15.84%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -11,15 +11,17 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null) return false;
-        if(head.next == null) return false;
-        HashSet<ListNode> set = new HashSet<ListNode>();
-        
-        while(true){
-            if(set.contains(head)) return true;
-            set.add(head);
-            head = head.next;
-            if(head == null) return false;
+        ListNode fast = head, slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
         }
+
+        return false;
     }
 }
