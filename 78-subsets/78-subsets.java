dc2e5a484e957762b774831db7eba1e4c58d4ea1
seokhyeonChung <44970596+seokhyeonChung@users.noreply.github.com>
2022-03-11 16:58:12 +0900

    Time: 1 ms (82.40%), Space: 43.7 MB (21.39%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,19 +1,28 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        dfs(nums, result, new ArrayList<>(), 0);
-        
-        return result;
-        
+  List<List<Integer>> output = new ArrayList();
+  int n, k;
+
+  public void backtrack(int first, ArrayList<Integer> curr, int[] nums) {
+    // if the combination is done
+    if (curr.size() == k) {
+      output.add(new ArrayList(curr));
+      return;
     }
-    
-    public void dfs(int[] nums, List<List<Integer>> result, List<Integer> list , int start){
-        result.add(new ArrayList<>(list));
-        
-        for(int i = start; i<nums.length; i++){
-            list.add(nums[i]);
-            dfs(nums, result, list, i +1);
-            list.remove(list.size() -1);
-        }
+    for (int i = first; i < n; ++i) {
+      // add i into the current combination
+      curr.add(nums[i]);
+      // use next integers to complete the combination
+      backtrack(i + 1, curr, nums);
+      // backtrack
+      curr.remove(curr.size() - 1);
     }
+  }
+
+  public List<List<Integer>> subsets(int[] nums) {
+    n = nums.length;
+    for (k = 0; k < n + 1; ++k) {
+      backtrack(0, new ArrayList<Integer>(), nums);
+    }
+    return output;
+  }
 }
