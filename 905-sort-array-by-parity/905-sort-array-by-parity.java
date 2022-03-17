d427ccbf340f87817cc53931d9c9411aaab8108b
seokhyeonChung <44970596+seokhyeonChung@users.noreply.github.com>
2022-03-17 22:07:15 +0900

    Time: 1 ms (96.10%), Space: 48.4 MB (30.39%) - LeetHub

---
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int left = 0;
+        int right = nums.length -1;
+        
+        while(left <right){
+            if(nums[left] % 2 == 0){
+                left++;
+            } else{
+                if(nums[right] % 2 == 0){
+                    int temp = nums[right];
+                    nums[right] = nums[left];
+                    nums[left] = temp;
+                }
+                right--;
+            }
+        }
+        return nums;
+    }
+}
